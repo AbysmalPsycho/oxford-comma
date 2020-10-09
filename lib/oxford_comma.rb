9ee8deeb300c78187874fac369d0a0ecd52182cc
AbysmalPsycho <rischthefisch@gmail.com>
2020-10-09 11:32:53 -0700

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+     if array.length == 1
+        array.join
+     elsif array.length == 2 
+        array.join(" and ")
+     else
+        array.collect do |x|
+            if x == array[-1]
+                array[-1] = "and " << x 
+            end
+        end
+        array.join(", ")
+    end
 end
